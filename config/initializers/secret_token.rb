0d739f62a9cd52c24f4dc4cac11acfb9153fdff5
Phil Morle <phil@pollenizer.com>
2014-08-29 07:53:19 +0630

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worldfarm::Application.config.secret_token = 'b96ae9056398522b444939a9d86b5ffe26736f3bee8f50e2b70c416df0ebc0287e11d6fb55e6c983646997c40e14b672bbbe3d53aea5a60be84956985dfe6f70'
